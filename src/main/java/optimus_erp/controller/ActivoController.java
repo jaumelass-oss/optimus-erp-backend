8e9c3c5c854d15842a22a8de706a67aff1b4da05
Janaco <jaume093@gmail.com>
2026-04-15 22:47:33 +0200

    implementation stock

--- src/main/java/optimus_erp/controller/ActivoController.java
+++ src/main/java/optimus_erp/controller/ActivoController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/activos")
+
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/activos")
+@RestController
 public class ActivoController {
 
     @Autowired
--- src/main/java/optimus_erp/model/Activo.java
+++ src/main/java/optimus_erp/model/Activo.java
@@ -1,5 +1,6 @@
 package optimus_erp.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,12 +14,17 @@ public class Activo {
     private Long id;
 
     private String nombre;
+
+    @Column(name = "numero_serie")
+    @JsonProperty("numero_serie")
     private String numeroSerie;
 
     @Enumerated(EnumType.STRING)
     private TipoActivo tipo;
 
     private Double valor;
+
+    private int stock;
 
     @Column(name = "empleado_id")
     private Integer empleadoId;
