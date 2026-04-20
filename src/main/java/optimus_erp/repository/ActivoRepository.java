024620a0faf6d4db3d7cfee7a2275d67f58b5883
Janaco <jaume093@gmail.com>
2026-04-20 20:01:12 +0200

    repositorio

--- src/main/java/optimus_erp/repository/ActivoRepository.java
+++ src/main/java/optimus_erp/repository/ActivoRepository.java
@@ -2,7 +2,9 @@ package optimus_erp.repository;
 
 import optimus_erp.model.Activo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivoRepository extends JpaRepository<Activo, Long> {
 
 }
--- src/main/java/optimus_erp/repository/UsuarioRepository.java
+++ src/main/java/optimus_erp/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package optimus_erp.repository;
 
 import optimus_erp.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByUsername(String username);
 }
